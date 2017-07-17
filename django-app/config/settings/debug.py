77f6631667f78e6c87114a525ebacbbd9607b04e
JeongEuiJin <hehar1020@gmail.com>
2017-07-18 02:01:17 +0900

    오탈자 수정 in setting.base ec2 연동하여 ec2에 올림(ubuntu)

--- django-app/config/settings/debug.py
+++ django-app/config/settings/debug.py
@@ -4,5 +4,4 @@ config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
