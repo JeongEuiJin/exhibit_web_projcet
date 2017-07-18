d75a14f70375a7a2b2c94caa3071045da11a4d55
JeongEuiJin <hehar1020@gmail.com>
2017-07-18 19:15:35 +0900

    debug : false change

--- django-app/config/settings/deploy.py
+++ django-app/config/settings/deploy.py
@@ -3,6 +3,6 @@ from .base import *
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
