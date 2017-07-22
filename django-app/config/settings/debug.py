75749b384c591de59b44182817d321f13605f1a5
JeongEuiJin <hehar1020@gmail.com>
2017-07-22 21:56:15 +0900

    config.url의 static경로 설정

--- django-app/config/settings/debug.py
+++ django-app/config/settings/debug.py
@@ -2,6 +2,11 @@ from .base import *
 
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
+# URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
--- django-app/config/settings/deploy.py
+++ django-app/config/settings/deploy.py
@@ -2,6 +2,10 @@ from .base import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
+# URLs
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
