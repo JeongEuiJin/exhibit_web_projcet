01d55c845af426a7a5c8731fcbd9a9bf15552982
JeongEuiJin <hehar1020@gmail.com>
2017-07-25 23:39:47 +0900

    aws 연동에서 db부분을 postsql로 변경하여 사용할수있게 변경

--- django-app/config/settings/debug.py
+++ django-app/config/settings/debug.py
@@ -10,3 +10,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- django-app/config/settings/deploy.py
+++ django-app/config/settings/deploy.py
@@ -10,3 +10,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 DEBUG = False
 
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = config_secret_deploy['django']['databases']
