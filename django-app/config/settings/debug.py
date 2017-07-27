0f1a0a98a0fbdbb8e9fdfc81e7382fbaa1d54bde
JeongEuiJin <hehar1020@gmail.com>
2017-07-27 11:57:08 +0900

    statci_root 추가 할수있게 작성

--- django-app/config/settings/debug.py
+++ django-app/config/settings/debug.py
@@ -4,11 +4,11 @@ config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
 # URLs
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
 
 # Database
--- django-app/config/settings/deploy.py
+++ django-app/config/settings/deploy.py
@@ -4,7 +4,7 @@ config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
 
 # URLs
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
